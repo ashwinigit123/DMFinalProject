3fc30ad3e2ed64ed1dc242f037527aed5cb1703d
Ashwini Trale <ashwinitrale8@gmail.com>
2021-05-01 23:21:09 +0000

    added model and csv

--- flask_app.py
+++ flask_app.py
@@ -2,7 +2,7 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, redirect, render_template, request, url_for
-
+import model
 app = Flask(__name__)
 comments = []
 app.config["DEBUG"] = True
@@ -12,6 +12,11 @@ def index():
     if request.method == "GET":
         return render_template("main_page.html", comments=comments)
 
-    comments.append(request.form["contents"])
+
+    recom = model.newRecommendations_50(request.form["contents"])
+    comments.append(recom)
     return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run()
 
